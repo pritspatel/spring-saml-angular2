97ddcab520a837e83b01fbd297e4062217d45fdc
Pritesh Patel <3ppatel@gmail.com>
2017-02-11 09:18:08 -0500

    -- upgraded angular -- fixed api config

--- src/main/java/com/prits/samlapp/api/GreetingsController.java
+++ src/main/java/com/prits/samlapp/api/GreetingsController.java
@@ -1,5 +1,7 @@
 package com.prits.samlapp.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,11 +9,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api")
 public class GreetingsController {
+	private static final Logger logger = LoggerFactory.getLogger(GreetingsController.class);
 
 	@RequestMapping(value = "/greetings",produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
 	public @ResponseBody Greeting sayHello(){
+		logger.info("Serving greetings controller.....");
 		Greeting g = new Greeting("Hello SAML");
 		return g;
 	}
